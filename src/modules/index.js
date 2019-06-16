4b055f0e43515a4e27ab6dbcb794827cb94d26d0
Artem Puzakov <apuzakov@awardwallet.com>
2019-06-16 12:06:10 +0500

    layout part

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,12 +1,14 @@
 import { combineReducers } from "redux";
 import { fork } from "redux-saga/effects";
 import auth, { sagas as authSaga } from "./Auth";
+import admin from "./Admin";
 // import map, { sagas as mapSaga } from "./Map";
 import { reducer as formReducer } from "redux-form";
 
 export default combineReducers({
   auth,
   form: formReducer,
+  admin,
   // map
 });
 
